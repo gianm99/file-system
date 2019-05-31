be855042018ce9eaac76f992ce9ddfe5412462ec
Gian Lucas <45042897+gianm99@users.noreply.github.com>
2019-05-31 17:21:34 +0200

    Cambios en la sangría

--- bloques.c
+++ bloques.c
@@ -13,24 +13,24 @@ static int descriptor = 0;
 */
 int bmount(const char *camino)
 {
-   if (descriptor > 0)
-   {
-      close(descriptor);
-   }
-   if ((descriptor = open(camino, O_RDWR | O_CREAT, 0666)) == -1)
-   {
-      fprintf(stderr, "Error: bloques.c → bmount() → open()\n");
-   }
-   if (!mutex) //mutex == 0
-   {
-      //el semáforo es único y sólo se ha de inicializar una vez en nuestro sistema (lo hace el padre)
-      mutex = initSem(); //lo inicializa a 1
-      if (mutex == SEM_FAILED)
-      {
-         return -1;
-      }
-   }
-   return descriptor;
+	if (descriptor > 0)
+	{
+		close(descriptor);
+	}
+	if ((descriptor = open(camino, O_RDWR | O_CREAT, 0666)) == -1)
+	{
+		fprintf(stderr, "Error: bloques.c → bmount() → open()\n");
+	}
+	if (!mutex) //mutex == 0
+	{
+		//el semáforo es único y sólo se ha de inicializar una vez en nuestro sistema (lo hace el padre)
+		mutex = initSem(); //lo inicializa a 1
+		if (mutex == SEM_FAILED)
+		{
+			return -1;
+		}
+	}
+	return descriptor;
 }
 
 /*-Función: Bumount.
@@ -41,15 +41,15 @@ int bmount(const char *camino)
 */
 int bumount()
 {
-   descriptor = close(descriptor);
-   // hay que asignar el resultado de la operación a la variable ya que bmount() la utiliza
-   if (descriptor == -1)
-   {
-      fprintf(stderr, "Error: bloques.c → bumount() → close(): %d: %s\n", errno, strerror(errno));
-      return -1;
-   }
-   deleteSem(); // borramos semaforo
-   return 0;
+	descriptor = close(descriptor);
+	// hay que asignar el resultado de la operación a la variable ya que bmount() la utiliza
+	if (descriptor == -1)
+	{
+		fprintf(stderr, "Error: bloques.c → bumount() → close(): %d: %s\n", errno, strerror(errno));
+		return -1;
+	}
+	deleteSem(); // borramos semaforo
+	return 0;
 }
 
 /*-Función: bwrite
@@ -62,18 +62,18 @@ int bumount()
 */
 int bwrite(unsigned int nbloque, const void *buf)
 {
-   int nbytes;
-   if (lseek(descriptor, BLOCKSIZE * nbloque, SEEK_SET) < 0)
-   {
-      fprintf(stderr, "ERROR %d: %s\n", errno, strerror(errno));
-      return -1;
-   }
-   if ((nbytes = write(descriptor, buf, BLOCKSIZE)) < 0)
-   {
-      fprintf(stderr, "ERROR %d: %s\n", errno, strerror(errno));
-      return -1;
-   }
-   return nbytes;
+	int nbytes;
+	if (lseek(descriptor, BLOCKSIZE * nbloque, SEEK_SET) < 0)
+	{
+		fprintf(stderr, "ERROR %d: %s\n", errno, strerror(errno));
+		return -1;
+	}
+	if ((nbytes = write(descriptor, buf, BLOCKSIZE)) < 0)
+	{
+		fprintf(stderr, "ERROR %d: %s\n", errno, strerror(errno));
+		return -1;
+	}
+	return nbytes;
 }
 /*-Función: Bread.
 **-Descripción: Lee el bloque especificado y copia su contenido en el buffer
@@ -84,34 +84,34 @@ int bwrite(unsigned int nbloque, const void *buf)
 */
 int bread(unsigned int nbloque, void *buf)
 {
-   int nbytes;
-   if (lseek(descriptor, BLOCKSIZE * nbloque, SEEK_SET) < 0)
-   {
-      fprintf(stderr, "ERROR %d: %s\n", errno, strerror(errno));
-      return -1;
-   }
-   if ((nbytes = read(descriptor, buf, BLOCKSIZE)) < 0)
-   {
-      fprintf(stderr, "ERROR %d: %s\n", errno, strerror(errno));
-      return -1;
-   }
-   return nbytes;
+	int nbytes;
+	if (lseek(descriptor, BLOCKSIZE * nbloque, SEEK_SET) < 0)
+	{
+		fprintf(stderr, "ERROR %d: %s\n", errno, strerror(errno));
+		return -1;
+	}
+	if ((nbytes = read(descriptor, buf, BLOCKSIZE)) < 0)
+	{
+		fprintf(stderr, "ERROR %d: %s\n", errno, strerror(errno));
+		return -1;
+	}
+	return nbytes;
 }
 
 void mi_waitSem()
 {
-   if (!inside_sc)
-   {
-      waitSem(mutex);
-   }
-   inside_sc++;
+	if (!inside_sc)
+	{
+		waitSem(mutex);
+	}
+	inside_sc++;
 }
 
 void mi_signalSem()
 {
-   inside_sc--;
-   if (!inside_sc)
-   {
-      signalSem(mutex);
-   }
+	inside_sc--;
+	if (!inside_sc)
+	{
+		signalSem(mutex);
+	}
 }
